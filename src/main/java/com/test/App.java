dadcd1e5deb96dc07f1adacb1153aaa80550edb4
Anand <anandchristal@yahoo.com>
2017-07-14 20:08:06 +0530

    commit testing 2

--- src/main/java/com/test/App.java
+++ src/main/java/com/test/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hellsfsdfsdfo World!" );
+        System.out.println( "Hellsfsdfsdfo Testing 2 World!" );
     }
 }
